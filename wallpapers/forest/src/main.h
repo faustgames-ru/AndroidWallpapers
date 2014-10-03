333729052cc16108fe89967c79a683cf5b06880b
neposeda <the.neposeda@gmail.com>
2014-10-03 14:19:13 +0300

    asteroids test

--- wallpapers/forest/src/main.h
+++ wallpapers/forest/src/main.h
@@ -7,47 +7,15 @@ using namespace gameplay;
 /**
  * This is a mesh demo game for rendering Mesh.
  */
-class CharacterGame: public Game, public AnimationClip::Listener, public PhysicsCollisionObject::CollisionListener
+class AsteroidsTest: public Game
 {
 public:
     
     /**
      * Constructor.
      */
-    CharacterGame();
+	AsteroidsTest();
 
-    /**
-     * @see Game::keyEvent
-     */
-    void keyEvent(Keyboard::KeyEvent evt, int key);
-
-    /**
-     * @see Game::touchEvent
-     */
-    void touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex);
-
-    /**
-     * @see Game::mouseEvent
-     */
-    bool mouseEvent(Mouse::MouseEvent evt, int x, int y, int wheelDelta);
-
-    /**
-     * @see Game::gamepadEvent
-     */
-    void gamepadEvent(Gamepad::GamepadEvent evt, Gamepad* gamepad);
-
-    /**
-     * @see AnimationClip::Listener::animationEvent
-     */
-    void animationEvent(AnimationClip* clip, AnimationClip::Listener::EventType type);
-
-    /**
-     * @see PhysicsCollisionObject::CollisionListener::collisionEvent
-     */
-    void collisionEvent(PhysicsCollisionObject::CollisionListener::EventType type,
-                                    const PhysicsCollisionObject::CollisionPair& collisionPair,
-                                    const Vector3& contactPointA = Vector3::zero(),
-                                    const Vector3& contactPointB = Vector3::zero());
 protected:
 
     /**
@@ -74,18 +42,10 @@ private:
     
     bool initializeScene(Node* node);
     void initializeMaterial(Scene* scene, Node* node, Material* material);
-    void initializeCharacter();
-	void initializeChair();
+	void initializeAsteroids();
     void drawSplash(void* param);
     bool drawScene(Node* node, bool transparent);
     void play(const char* id, bool repeat, float speed = 1.0f);
-    void jump();
-    void kick();
-    void adjustCamera(float elapsedTime);
-    bool isOnFloor() const;
-    void clone();
-    void grabBall();
-    void releaseBall();
 
     Font* _font;
     Scene* _scene;
